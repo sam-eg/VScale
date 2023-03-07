fbb14c88f81b2cbc23ea4d886c9f5685c2767ec7
Sam Evans-Golden <Sam72eg@gmail.com>
2023-03-07 11:50:44 -0500

    Made cubes go zoom

---
+++ Assets/EngineScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineScript : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.AddForce(new Vector3(0, 0, 5));
+    }
+}
